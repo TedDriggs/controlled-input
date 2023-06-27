3610bf8adc2c50c1c2c4fe8ee5a53b76ebcd6254
Ted Driggs <ted.driggs@outlook.com>
2023-06-27 06:46:33 -0700

    Fix module-name casing issue

--- src/validation/validator.ts
+++ src/validation/validator.ts
@@ -1,12 +1,12 @@
 import flatMap from 'lodash-es/flatMap';
 
 import { ObjectPath } from './objectPath';
-import { Diagnostic } from './Diagnostic';
+import { Diagnostic } from './diagnostic';
 
 /**
  * A function that evaluates a value and returns a list of diagnostic messages
  * with relative property paths to where they occur in the input value.
- * 
+ *
  * @public
  */
 export type Validator<T> = (value: T) => Diagnostic[];
@@ -20,26 +20,30 @@ export const Validator = {
      *
      * @param validator The validator function to apply to each item
      */
-    forList: <T>(validator: Validator<T>): Validator<T[]> => items =>
-        flatMap(items, (v, i) => validateProperty(i, validator, v)),
+    forList:
+        <T>(validator: Validator<T>): Validator<T[]> =>
+        items =>
+            flatMap(items, (v, i) => validateProperty(i, validator, v)),
     /**
      * Create a new validator which runs all the provided validators and returns
      * the combined results.
      *
      * @param validators A set of independently-run validators
      */
-    combine: <T>(...validators: readonly Validator<T>[]): Validator<T> => val =>
-        flatMap(validators, fn => fn(val)),
+    combine:
+        <T>(...validators: readonly Validator<T>[]): Validator<T> =>
+        val =>
+            flatMap(validators, fn => fn(val)),
 };
 
 /**
- * Run a validator against some value, then update the path of the diagnostics 
+ * Run a validator against some value, then update the path of the diagnostics
  * to reflect their location in some parent object.
- * 
+ *
  * @param propName The value to prepend to the path of any returned diagnostics
  * @param validator The validator to run
  * @param value The value to check
- * 
+ *
  * @beta
  */
 // This is not stable because the API is kind of clunky at the moment.
